c2dc4092deeda37d56d82d43612d0ac1a7b1aebb
tdfellows <tdfellows@5540a886-1149-11df-83e8-2f775389245c>
2010-03-26 08:37:58 +0000

    Removed Poison Arrow

--- trunk/KingsDamageMeter/KingsDamageMeter/Properties/Settings.Designer.cs
+++ trunk/KingsDamageMeter/KingsDamageMeter/Properties/Settings.Designer.cs
@@ -155,7 +155,7 @@ namespace KingsDamageMeter.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Debug {
             get {
                 return ((bool)(this["Debug"]));
